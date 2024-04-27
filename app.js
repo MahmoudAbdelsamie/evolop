7d9b037150de959d3c5cf06f039707e7f6987722
MahmoudAbdelsamie <msomaa38@gmail.com>
2024-04-27 10:23:59 +0300

    Add cors and helmet middlewares to the api

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
 require('dotenv').config();
 
 
@@ -15,7 +17,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(helmet());
+app.use(cors());
 
 app.use((req, res, next) => {
     User
